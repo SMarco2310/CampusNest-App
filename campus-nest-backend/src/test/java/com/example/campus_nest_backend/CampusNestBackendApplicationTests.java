dcb8e9d700f1166a49335c9c1160313276b1996c
Marc-Etienne sossou <marc.sossou@ashesi.edu.gh>
2025-07-25 12:54:12 +0000

    initialized the project backend

---
+++ campus-nest-backend/src/test/java/com/example/campus_nest_backend/CampusNestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campus_nest_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusNestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
