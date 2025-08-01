6a05b3d3c6705611940445a650ae399f86e97cfb
Marc-Etienne sossou <marc.sossou@ashesi.edu.gh>
2025-08-01 02:12:17 +0000

    Started the front-end by adding  the browser Route systemand the components, pages, layout folders that will containe all theose elements

--- campus-nest-frontend/campus-nest-frontend/src/App.jsx
+++ campus-nest-frontend/campus-nest-frontend/src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
